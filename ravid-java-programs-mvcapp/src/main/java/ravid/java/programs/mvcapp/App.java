7affde02e7f6c07e3cd5c3dbab6856e83b337561
Ravi Dontharaju <ravi.dontharaju@gmail.com>
2012-04-03 00:01:27 -0700

    What: - Added project for future MVC app.

---
+++ ravid-java-programs-mvcapp/src/main/java/ravid/java/programs/mvcapp/App.java
@@ -0,0 +1,13 @@
+package ravid.java.programs.mvcapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
